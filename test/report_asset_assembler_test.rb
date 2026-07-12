d08edcaf9704f029ff522343195fe4b7cc25ac14
st0012 <stan001212@gmail.com>
2026-07-12 10:58:56 +0100

    Keep focused atlas ranges visible

--- test/report_asset_assembler_test.rb
+++ test/report_asset_assembler_test.rb
@@ -5,7 +5,7 @@ require_relative "test_helper"
 
 class ReportAssetAssemblerTest < Minitest::Test
   # SHA-256 of the supported Explorer shell with the shared canonical Showcase runtime.
-  REPORT_HTML_SHA256 = "adb47e991eda2510a039612b231a7b1519f70b6c7c84bc0ab4759423a4fdf342"
+  REPORT_HTML_SHA256 = "fb5d0b0792a384e95d8b23eac883f28e97d449c554a064c0d5ebbd2264d7dd78"
 
   def test_assembles_the_supported_explorer_assets_byte_for_byte
     assert_equal(
